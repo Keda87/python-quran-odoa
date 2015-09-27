73a309e2a6c3f7e013517b11823dcd0b2ec74e60
“Keda87” <adiyatmubarak@gmail.com>
2015-09-27 10:43:31 +0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='utility development quran',
+    keywords='development quran',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
