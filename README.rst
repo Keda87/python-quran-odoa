5ce34ab961f384375a23b45e59777df467d57093
“Keda87” <adiyatmubarak@gmail.com>
2015-09-27 10:09:24 +0700

    Initial setup

---
+++ README.rst
@@ -0,0 +1,6 @@
+## Python Quran ODOA
+
+Simple library to support ODOA (One Day One Ayat) campaign that will displaying
+random ayat including the translation.
+
+Suitable for your bot or web apps.
---
+++ setup.py
@@ -0,0 +1,62 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='python-quran-odoa',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.2.0',
+
+    description='Library to get random ayat including the translation.',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/Keda87/python-quran-odoa',
+
+    # Author details
+    author='Adiyat Mubarak',
+    author_email='adiyatmubarak@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Utility',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+
+    # What does your project relate to?
+    keywords='utility development quran',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+)
